f3a8465468644025329d3f14f801c14463d99574
zhguzya <jack.i@mail.ru>
2025-09-22 20:13:01 +0300

    6 commit

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,14 @@
-bind = "0.0.0.0:8181"
-workers = 3
+# bind = "0.0.0.0:8181"
+# workers = 3
+
+
+
+import multiprocessing
+
+bind = "unix:/home/zhguzya/my_app/my_app.sock"
+workers = multiprocessing.cpu_count() * 2 + 1
+umask = 0o007
+wsgi_app = "api:app"
 accesslog = "-"
 errorlog = "-"
 loglevel = "info"
