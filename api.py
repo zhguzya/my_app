6852b5ab22e80ff5dbe3d434005334927554b62d
zhguzya <jack.i@mail.ru>
2025-09-22 20:49:31 +0300

    9 commit

--- api.py
+++ api.py
@@ -10,4 +10,5 @@ class HelloWorld(Resource):
         return {'msg': 'hello, world'}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8181, debug = True)
+    # app.run(host='0.0.0.0', port=8181, debug = True)
+    app.run(debug = True)
