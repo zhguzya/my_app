5bd81e4eaddd8abf15645224887e483690806de7
zhguzya <jack.i@mail.ru>
2025-09-22 20:20:05 +0300

    7 commit

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,12 +1,15 @@
 import multiprocessing
+import os
 
 # bind = "0.0.0.0:8181"
 # workers = 3
 
-bind = "unix:/home/zhguzya/my_app/my_app.sock"
+BASE_DIR = "/home/zhguzya/my_app"
+
+bind = f"unix:{BASE_DIR}/my_app.sock"
 workers = multiprocessing.cpu_count() * 2 + 1
 umask = 0o007
 wsgi_app = "api:app"
-accesslog = "-"
-errorlog = "-"
+accesslog = f"{BASE_DIR}/logs/gunicorn_access.log"
+errorlog  = f"{BASE_DIR}/logs/gunicorn_error.log"
 loglevel = "info"
