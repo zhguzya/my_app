4b5b2549d397ea2b7a589dae4d46a422fbea96ae
zhguzya <jack.i@mail.ru>
2025-09-22 20:14:17 +0300

    7 commit

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,9 +1,7 @@
+import multiprocessing
+
 # bind = "0.0.0.0:8181"
 # workers = 3
-
-
-
-import multiprocessing
 
 bind = "unix:/home/zhguzya/my_app/my_app.sock"
 workers = multiprocessing.cpu_count() * 2 + 1
