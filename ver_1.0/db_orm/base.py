6d2da229bb0a508b93f48753da2a0eba67bc1afc
zhguzya <jack.i@mail.ru>
2026-02-12 17:55:40 +0300

    test

--- ver_1.0/db_orm/base.py
+++ ver_1.0/db_orm/base.py
@@ -4,10 +4,10 @@ from config import DATABASE_NAME_ORM, DATABASE_NAME_USERS
 
 #mikrotik_db
 BaseMikrotik = declarative_base()
-engine_mikrotik = create_engine(f'sqlite:///{DATABASE_NAME_ORM}', echo=True)#унести в config
+engine_mikrotik = create_engine(f'sqlite:///{DATABASE_NAME_ORM}', echo=True)#1унести в config
 SessionMikrotik = sessionmaker(bind=engine_mikrotik)
 
 #users_db
 BaseUsers = declarative_base()
-engine_users = create_engine(f'sqlite:///{DATABASE_NAME_USERS}', echo=True)#унести в config
+engine_users = create_engine(f'sqlite:///{DATABASE_NAME_USERS}', echo=True)
 SessionUsers = sessionmaker(bind=engine_users)
